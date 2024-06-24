a74151b496b07a0cdbd3d893a563c2ee2e4be64a
FABRICA <FABRICA@FABRICA.com>
2024-06-24 17:29:05 -0600

    firs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { isPlatformBrowser } from "@angular/common";
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
